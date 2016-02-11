142b01132bc42b315a74c6870bfa1f43b7fa86c0
ericslc <eric.leatham@gmail.com>
2016-02-10 22:04:08 -0700

    work in progress

--- instanceArray.js
+++ instanceArray.js
@@ -14,17 +14,18 @@ var User = function(name, email, pw){
 //Create an Array called 'users' that will store all our instances of User.
 
   //code here
-  var users = [];
+  var users = new Array(user);
 
 
 //Now create and push into your users array 3 separate instances of User using the data from above in that exact order
 
   //code here
-
+var user1 = users.push(Tyler, tylermcginnis33@gmail.com, 'iLoveJS');
 console.log('Tyler\'s information is ');
 //Console.log all of Tylers information
 
   //code here
+var user2 = users.push('Cahlan', 'cahlan@devmounta.in', 'iLoveHashtags');
 
 console.log('Lenny\'s information is ');
 //Now console.log all of Lennys information
@@ -35,7 +36,7 @@ console.log('Lenny\'s information is ');
 //Now create another instance of User using your own information and then add that to your users array.
 
   //code here
-
+var user3 = users.push('Eric', 'eric@devmounta.in', 'iamokwithhashtags');
 console.log('All my users names are ');
 //Now loop through your users Array and console.log every users name.
 
